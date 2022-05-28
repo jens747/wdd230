cfb1858f804749f90dc79b6c3b0ac5ec7cf4b67d
jens747 <jjens7113@gmail.com>
2022-05-28 17:19:42 -0600

    added windchill.js and Stormglass API to get current weather status

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -4,8 +4,6 @@ const lng = -111.1005;
 const params = "airTemperature,windSpeed";
 
 let weatherData = {};
-let tempC = 16.96;
-let windSpeedMps = 5.73;
 
 // const url = `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`, {
 //   headers: {
@@ -21,32 +19,87 @@ let windSpeedMps = 5.73;
 //   }
 // }
 
-// fetch(
-//   `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`,
-//   {
-//     headers: {
-//       Authorization:
-//         "e186e4f0-dd6e-11ec-bcd6-0242ac130002-e186e55e-dd6e-11ec-bcd6-0242ac130002",
-//     },
-//   }
-// )
-//   .then((response) => response.json())
-//   .then((jsonData) => {
-//     weatherData = jsonData;
-//     console.log(weatherData);
-//     windSpeedMps = weatherData.hours[0].windSpeed.noaa;
-//     tempC = weatherData.hours[0].airTemperature.noaa;
-//   });
+// *Using the Stormglass API to get current weather status
+fetch(
+  `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`,
+  {
+    headers: {
+      Authorization:
+        "e186e4f0-dd6e-11ec-bcd6-0242ac130002-e186e55e-dd6e-11ec-bcd6-0242ac130002",
+    },
+  }
+)
+  .then((response) => response.json())
+  .then((jsonData) => {
+    weatherData = jsonData;
+    console.log(weatherData);
 
-const windSpeedMph = windSpeedMps / 0.44704;
-console.log(`Wind speed: mps - ${windSpeedMps}, mph - ${windSpeedMph}`);
+    output(weatherData);
+  });
 
-const tempF = tempC * 1.8 + 32;
-console.log(`Temperature: C - ${tempC}, F - ${tempF}`);
+// **For debugging without Stormglass API
+// const output = () => {
 
-const windChill =
-  35.74 +
-  0.6215 * tempF -
-  35.75 * windSpeedMph * 0.16 +
-  0.4275 * tempF * windSpeedMph * 0.16;
-console.log(`Wind chill: ${windChill} F`);
+// *Using the Stormglass API
+const output = (data) => {
+  // **For debugging without Stormglass API
+  // const tempC = 6.96;
+  // const windSpeedMps = 5.73;
+
+  // *Using the Stormglass API
+  const windSpeedMps = data.hours[0].windSpeed.noaa;
+  const tempC = data.hours[0].airTemperature.noaa;
+
+  // const windSpeedKph = windSpeedMpsToKph(windSpeedMps);
+  const tempF = tempCtoF(tempC);
+  const windSpeedMph = windSpeedMpsToMph(windSpeedMps);
+
+  document.querySelector("#temp").textContent = `${tempF.toFixed(0)} °F`;
+  document.querySelector("#windSpeedH4").textContent = `${windSpeedMph.toFixed(
+    2
+  )} mph`;
+
+  let windChillH4 = document.querySelector("#windChillH4");
+  windChillH4.textContent = getWindChill(windSpeedMph, tempF);
+};
+
+// Define the windSpeedMpsToMph function
+const windSpeedMpsToMph = (mps) => mps / 0.44704;
+
+// Define the windSpeedMpsToKph function
+const windSpeedMpsToKph = (mps) => mps * 3.6;
+
+// Convert km/h to mph
+// const kphToMph = (kph) => kph * 0.621371;
+
+// Convert mph to km/h
+// const mphToKph = (mph) => mph / 0.621371;
+
+// Convert Celsius to Fahrenheit
+const tempCtoF = (cel) => cel * 1.8 + 32;
+
+// Convert Fahrenheit to Celsius
+const tempFtoC = (fahr) => (fahr - 32) / 1.8;
+
+// Calculate the Wind Chill Factor
+const getWindChill = (mph, temp) => {
+  // Set value of windChill to "N/A"
+  let windChill = "N/A";
+
+  // Only calculate wind chill if wind is strong enough and temp is low enough
+  if (mph > 3.0 && temp <= 50) {
+    windChill = (
+      35.74 +
+      0.6215 * temp -
+      35.75 * mph ** 0.16 +
+      0.4275 * temp * mph ** 0.16
+    ).toFixed(2);
+    // console.log(`Wind chill: ${windChill} °F`);
+    windChill = `${windChill} °F`;
+  }
+
+  return windChill;
+};
+
+// **For debugging without Stormglass API
+// output();
