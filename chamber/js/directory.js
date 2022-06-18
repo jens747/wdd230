6109939985cbb3df88383428542e7b96901e8bc7
jens747 <jjens7113@gmail.com>
2022-06-18 16:30:14 -0600

    adding chamber directory page

---
+++ chamber/js/directory.js
@@ -0,0 +1,66 @@
+const cards = document.querySelector(".cards");
+
+const fetchJSON = async () => {
+  try {
+    const jsonURL = "json/data.json";
+    const req = new Request(jsonURL);
+    const res = await fetch(req);
+    const jsonObject = await res.json();
+    const businesses = jsonObject["businesses"];
+
+    console.log(businesses);
+
+    businesses.forEach(displayCards);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchJSON();
+
+const displayCards = (cardObj) => {
+  let card = document.createElement("section");
+  let name = document.createElement("h2");
+  let category = document.createElement("h3");
+  let addr = document.createElement("p");
+  let phone = document.createElement("p");
+  let url = document.createElement("a");
+  let member = document.createElement("p");
+  let logo = document.createElement("img");
+
+  logo.setAttribute("src", cardObj.logo);
+  logo.setAttribute("alt", `Logo image for ${cardObj.name}.`);
+  logo.setAttribute("loading", "lazy");
+
+  name.textContent = `${cardObj.name}`;
+  name.className = "dir-name";
+
+  category.textContent = `${cardObj.category}`;
+  category.className = "dir-category";
+
+  url.textContent = `${cardObj.url}`;
+  url.className = "dir-url";
+  url.href = `${cardObj.url}`;
+  url.target = "_blank";
+  url.rel = "noopener noreferrer";
+
+  addr.textContent = `${cardObj.address}`;
+  addr.className = "dir-addr";
+
+  phone.textContent = `Phone: ${cardObj.phone}`;
+  phone.className = "dir-phone";
+
+  member.textContent = `${cardObj.member} member`;
+  member.className = `dir-member ${(cardObj.member).toLowerCase()}`;
+
+
+  card.appendChild(logo);
+  card.appendChild(name);
+  card.appendChild(category);
+  card.appendChild(url);
+  card.appendChild(addr);
+  card.appendChild(phone);
+  card.appendChild(member);
+
+  document.querySelector(".dir-cards").appendChild(card);
+}
--- lesson9/prophets/js/prophets.js
+++ lesson9/prophets/js/prophets.js
@@ -1,62 +1,83 @@
-const requestURL = "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
+const requestURL =
+  "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
 
 const cards = document.querySelector(".cards");
+
+// async function getProphets() {
+//   let res = await fetch(URL);
+//   if(res.ok) {
+//     let data = await res.json();
+
+//     displayProphets(data);
+//   } else {
+//     throw Error(res.statusText);
+//   }
+// }
+
+// getProphets();
 
 fetch(requestURL)
   .then(function (res) {
     return res.json();
   })
   .then(function (jsonObj) {
-    console.table(jsonObj)
-    const prophets = jsonObj['prophets'];
+    console.table(jsonObj);
+    const prophets = jsonObj["prophets"];
 
     prophets.forEach(displayProphets);
+    // Alternative to filter prophets
+    // let nonUtah = data.prophets.filter(p => p.birthplace !== "Utah");
+    // nonUtah.forEach(displayProphets);
   });
 
-  function displayProphets(prophet) {
-    // Create elements to add to the document
-    let card = document.createElement("section");
-    let h2 = document.createElement("h2");
-    let portrait = document.createElement("img");
-    let birth = document.createElement("p");
+function displayProphets(prophet) {
+  // Create elements to add to the document
+  let card = document.createElement("section");
+  let h2 = document.createElement("h2");
+  let portrait = document.createElement("img");
+  let birth = document.createElement("p");
 
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${prophet.name} ${prophet.lastname}`;
-    
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute("src", prophet.imageurl);
-    portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}${numberSuffix(prophet.order)} Latter-day President`);
-    portrait.setAttribute("loading", "lazy");
+  // Change the textContent property of the h2 element to contain the prophet's full name
+  h2.textContent = `${prophet.name} ${prophet.lastname}`;
 
-    birth.textContent = `Born in ${prophet.birthplace} on ${prophet.birthdate}`;
-    
-    // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
+  // If you use HTML in your string call innerHTML otherwise textContent
+  // h2.innerHTML = `${prophet.name} <span class="highlight">{$prophet.lastname}</span>`;
 
-    // Add/append the section(card) with the img element
-    card.appendChild(portrait);
+  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+  portrait.setAttribute("src", prophet.imageurl);
+  portrait.setAttribute(
+    "alt",
+    `Portrait of ${prophet.name} ${prophet.lastname} - ${
+      prophet.order
+    }${numberSuffix(prophet.order)} Latter-day President`
+  );
+  portrait.setAttribute("loading", "lazy");
 
-    // Add/append the section(card) with the birth information
-    card.appendChild(birth);
+  birth.textContent = `Born in ${prophet.birthplace} on ${prophet.birthdate}`;
 
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector("div.cards").appendChild(card);
-  }
+  // Add/append the section(card) with the h2 element
+  card.appendChild(h2);
 
-  function numberSuffix(num) {
-    if(num == 1 || (num > 20 && num % 10 == 1))
-      return "st";
-    else if(num == 2 || (num > 20 && num % 10 == 2))
-      return "nd";
-    else if(num == 3 || (num > 20 && num % 10 == 3))
-      return "rd";
-    else
-      return "th";
-  }
+  // Add/append the section(card) with the img element
+  card.appendChild(portrait);
 
-  // Testing numberSuffix function
-  // numArray = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33];
+  // Add/append the section(card) with the birth information
+  card.appendChild(birth);
 
-  // numArray.forEach(num => {
-  //   console.log(`${num}${numberSuffix(num)}`)
-  // });
+  // Add/append the existing HTML div with the cards class with the section(card)
+  document.querySelector("div.cards").appendChild(card);
+}
+
+function numberSuffix(num) {
+  if (num == 1 || (num > 20 && num % 10 == 1)) return "st";
+  else if (num == 2 || (num > 20 && num % 10 == 2)) return "nd";
+  else if (num == 3 || (num > 20 && num % 10 == 3)) return "rd";
+  else return "th";
+}
+
+// Testing numberSuffix function
+// numArray = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33];
+
+// numArray.forEach(num => {
+//   console.log(`${num}${numberSuffix(num)}`)
+// });
