91ce77d060c770525c759e2ae138c694265d8f30
jens747 <jjens7113@gmail.com>
2022-07-17 15:30:12 -0600

    adding basic temple inn files, and index.html with header/footer

---
+++ temple-inn-and-suites/js/date.js
@@ -0,0 +1,86 @@
+// Obejct with day and month values
+const dateObj = {
+  // Array listing days of the week
+  weekdays: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+
+  // Array listing months of the year
+  months: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+};
+
+// Create a new date object
+const newDateObj = new Date();
+
+// Function to format current time
+const getCurrentTime = (newDateObj) => {
+  // Get current hours
+  let currentHour = newDateObj.getHours();
+
+  // Add leading zero to minutes if necessary
+  let currentMinute = newDateObj.getMinutes();
+  currentMinute = currentMinute <= 9 ? "0" + currentMinute : currentMinute;
+
+  // Check for AM or PM
+  const currentAMPM = currentHour >= 11 ? "PM" : "AM";
+
+  // Convert hours to 12 hour format
+  if (currentHour > 12) {
+    currentHour -= 12;
+  }
+
+  if (currentHour === 0) {
+    currentHour = 12;
+  }
+
+  // Return formatted string of time
+  return `${currentHour}:${currentMinute} ${currentAMPM}`;
+};
+
+// Variables for day, month, date, year, and time
+let currentDay = dateObj.weekdays[newDateObj.getDay()];
+let currentMonth = dateObj.months[newDateObj.getMonth()];
+let currentDayOf = newDateObj.getDate();
+let currentYear = newDateObj.getFullYear();
+let currentTime = getCurrentTime(newDateObj);
+let currentDateTime = ` ${currentDay}, ${currentMonth} ${currentDayOf}, ${currentYear} ${currentTime}`;
+
+// Variable used to insert date into webpage
+const copyrightYr = document.querySelector("#copyrightYr");
+copyrightYr.textContent = currentYear;
+
+// Variable to update the lastModified div
+const lastModified = document.querySelector("#lastModified");
+
+// Update the lastModified div with the current date
+lastModified.textContent += currentDateTime;
+
+// Update the current date and time
+function updateTime() {
+  const submitDateTime = new Date();
+  currentDay = dateObj.weekdays[submitDateTime.getDay()];
+  currentMonth = dateObj.months[submitDateTime.getMonth()];
+  currentDayOf = submitDateTime.getDate();
+  currentYear = submitDateTime.getFullYear();
+  currentTime = getCurrentTime(submitDateTime);
+  currentDateTime = ` ${currentDay}, ${currentMonth} ${currentDayOf}, ${currentYear} ${currentTime}`;
+}
